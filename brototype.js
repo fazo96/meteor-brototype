67c2e7b90540dc87477d287519912b3315ddae0c
fazo96 <fazius2009@gmail.com>
2014-10-10 12:02:15 +0200

    trying to make it work

--- brototype.js
+++ brototype.js
@@ -1,155 +1,152 @@
-/*global exports:true, module:true, window:true, require:false, define:false*/
-(function() {
-    'use strict';
+'use strict';
 
-    // Bromise... it's stronger than a Promise
-    function Bromise(object, method, args) {
-        this.object = object;
-        this.method = method;
-        this.args = args.length > 1 ? args.slice(1) : [];
-    }
+// Bromise... it's stronger than a Promise
+function Bromise(object, method, args) {
+    this.object = object;
+    this.method = method;
+    this.args = args.length > 1 ? args.slice(1) : [];
+}
 
-    Bromise.brototype = Bromise.prototype = {
-        "butWhenIdo": function(callback, context) {
-            if (this.method instanceof Function) {
-                var returnValue = this.method.apply(this.object, this.args);
-                if (returnValue) {
-                    (callback || function(){}).call(context || this.object, returnValue);
-                }
+Bromise.brototype = Bromise.prototype = {
+    "butWhenIdo": function(callback, context) {
+        if (this.method instanceof Function) {
+            var returnValue = this.method.apply(this.object, this.args);
+            if (returnValue) {
+                (callback || function(){}).call(context || this.object, returnValue);
             }
-            return context;
-        },
-
-        "hereComeTheErrors": function(callback) {
-            if (this.method instanceof Function) {
-                try {
-                    this.method.apply(this.object, this.args);
-                } catch(e) {
-                    callback(e);
-                }
-            } else {
-                callback(this.method + ' is not a function.');
-            }
-        },
-        "errorsAreComing": function () {
-            this.hereComeTheErrors.apply(this, arguments);
         }
-    };
+        return context;
+    },
 
-    function Bro(obj) {
-        if (this instanceof Bro) {
-            this.obj = obj;
+    "hereComeTheErrors": function(callback) {
+        if (this.method instanceof Function) {
+            try {
+                this.method.apply(this.object, this.args);
+            } catch(e) {
+                callback(e);
+            }
         } else {
-            return new Bro(obj);
+            callback(this.method + ' is not a function.');
         }
+    },
+    "errorsAreComing": function () {
+        this.hereComeTheErrors.apply(this, arguments);
     }
+};
 
-    Bro.TOTALLY = true;
-    Bro.NOWAY = false;
+function Bro(obj) {
+    if (this instanceof Bro) {
+        this.obj = obj;
+    } else {
+        return new Bro(obj);
+    }
+}
 
-    Bro.brototype = Bro.prototype = {
-        "isThatEvenAThing": function() {
-            return this.obj !== void 0;
-        },
+Bro.TOTALLY = true;
+Bro.NOWAY = false;
 
-        "doYouEven": function(key, options) {
-            var optionsBro = Bro(options || {}),
-                bro = this.iCanHaz(key);
-            if (Bro(bro).isThatEvenAThing() === Bro.TOTALLY) {
-                optionsBro.iDontAlways('forSure').butWhenIdo();
-                return Bro.TOTALLY;
-            } else {
-                optionsBro.iDontAlways('sorryBro').butWhenIdo();
-                return Bro.NOWAY;
-            }
-        },
+Bro.brototype = Bro.prototype = {
+    "isThatEvenAThing": function() {
+        return this.obj !== void 0;
+    },
 
-        "iCanHaz": function(key) {
-            if (Array.isArray(key)) {
-                var index, value, result = [];
-                for (index in key) {
-                    if (value = this.iCanHaz(key[index])) {
-                        result.push(value);
-                    }
-                }
-                return result;
-            }
-            var props = key.split('.'),
-                item = this.obj;
-            for (var i = 0; i < props.length; i++) {
-                item = item[props[i]];
-                if (Bro(item).isThatEvenAThing() === Bro.NOWAY) {
-                    return item;
-                }
-            }
-            return item;
-        },
+    "doYouEven": function(key, options) {
+        var optionsBro = Bro(options || {}),
+            bro = this.iCanHaz(key);
+        if (Bro(bro).isThatEvenAThing() === Bro.TOTALLY) {
+            optionsBro.iDontAlways('forSure').butWhenIdo();
+            return Bro.TOTALLY;
+        } else {
+            optionsBro.iDontAlways('sorryBro').butWhenIdo();
+            return Bro.NOWAY;
+        }
+    },
 
-        "comeAtMe": function(brobject) {
-            var i, prop,
-                bro = Bro(brobject),
-                keys = bro.giveMeProps(),
-                obj = (this instanceof Bro) ? this.obj : Bro.prototype;
-            for (i = 0; i < keys.length; i++) {
-                prop = keys[i];
-                if (bro.hasRespect(prop)) {
-                    obj[prop] = brobject[prop];
+    "iCanHaz": function(key) {
+        if (Array.isArray(key)) {
+            var index, value, result = [];
+            for (index in key) {
+                if (value = this.iCanHaz(key[index])) {
+                    result.push(value);
                 }
             }
-        },
+            return result;
+        }
+        var props = key.split('.'),
+            item = this.obj;
+        for (var i = 0; i < props.length; i++) {
+            item = item[props[i]];
+            if (Bro(item).isThatEvenAThing() === Bro.NOWAY) {
+                return item;
+            }
+        }
+        return item;
+    },
 
-        "giveMeProps": function() {
-            var key, props = [];
-            if (Object.keys) {
-                props = Object.keys(this.obj);
-            } else {
-                for (key in this.obj) {
-                    if (this.obj.hasRespect(key)) {
-                        props.push(key);
-                    }
+    "comeAtMe": function(brobject) {
+        var i, prop,
+            bro = Bro(brobject),
+            keys = bro.giveMeProps(),
+            obj = (this instanceof Bro) ? this.obj : Bro.prototype;
+        for (i = 0; i < keys.length; i++) {
+            prop = keys[i];
+            if (bro.hasRespect(prop)) {
+                obj[prop] = brobject[prop];
+            }
+        }
+    },
+
+    "giveMeProps": function() {
+        var key, props = [];
+        if (Object.keys) {
+            props = Object.keys(this.obj);
+        } else {
+            for (key in this.obj) {
+                if (this.obj.hasRespect(key)) {
+                    props.push(key);
                 }
             }
-            return props;
-        },
-        
-        "hasRespect": function(prop) {
-            return this.obj.hasOwnProperty(prop);
-        },
-        
+        }
+        return props;
+    },
 
-        "iDontAlways": function(methodString) {
-            var method = this.iCanHaz(methodString);
-            return new Bromise(this.obj, method, arguments);
-        },
+    "hasRespect": function(prop) {
+        return this.obj.hasOwnProperty(prop);
+    },
 
-        "braceYourself": function(methodString) {
-            var method = this.iCanHaz(methodString);
-            return new Bromise(this.obj, method, arguments);
-        }
-    };
 
-    try {
-        if (exports) {
-            exports.Bro = Bro;
-            return;
-        }
-    } catch(e) {}
-    try {
-        if (module) {
-            module.exports = Bro;
-            return;
-        }
-    } catch(e) {}
-    try {
-        if (require) {
-            define([], function() {return Bro;});
-            return;
-        }
-    } catch(e) {}
-    try {
-        if (window) {
-            window.Bro = Bro;
-            return;
-        }
-    } catch(e) {}
-})();
+    "iDontAlways": function(methodString) {
+        var method = this.iCanHaz(methodString);
+        return new Bromise(this.obj, method, arguments);
+    },
+
+    "braceYourself": function(methodString) {
+        var method = this.iCanHaz(methodString);
+        return new Bromise(this.obj, method, arguments);
+    }
+};
+
+try {
+    if (exports) {
+        exports.Bro = Bro;
+        return;
+    }
+} catch(e) {}
+try {
+    if (module) {
+        module.exports = Bro;
+        return;
+    }
+} catch(e) {}
+try {
+    if (require) {
+        define([], function() {return Bro;});
+        return;
+    }
+} catch(e) {}
+try {
+    if (window) {
+        window.Bro = Bro;
+        return;
+    }
+} catch(e) {}
--- package.js
+++ package.js
@@ -1,10 +1,12 @@
 Package.describe({
   summary: "Bro, do you even?",
-  version: "0.0.1",
+  version: "0.0.2",
   git: "https://github.com/fazo96/meteor-brototype"
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.3.1');
   api.addFiles('brototype.js');
+  api.export('Bro','server');
+  api.export('Bro','client');
 });
